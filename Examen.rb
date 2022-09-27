ab067a3c7c7ff51de292ad9da441bfe29d4a1690
CrisDaniel <daniel981311@gmail.com>
2022-09-27 02:03:08 -0500

    "Listos"

--- Examen.rb
+++ Examen.rb
@@ -1,3 +1,5 @@
+#Inicio de EXAMEN
+
 def number(num)
     num *= 2
     return num
@@ -5,8 +7,3 @@ end
 
 print number(68)
 puts " "
-print number(69)
-puts " "
-print number(70)
-puts " "
-print number(71)
